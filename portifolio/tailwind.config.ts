d7ba3b8a3ec277dd4d4ad9f103e689fec876fa1d
yanchagasPC <yanchagas2004@gmail.com>
2025-03-02 03:14:33 -0300

    feat: Header v1.0

--- portifolio/tailwind.config.ts
+++ portifolio/tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        "nunito-sans": ["var(--font-nunito-sans)"],
+      }
     },
   },
   plugins: [],
