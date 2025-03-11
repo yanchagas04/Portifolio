ddb6065bd575bc4c931913150e3e3f2b8c374a24
Yan Chagas <yanchagas2004@gmail.com>
2025-03-11 14:40:58 -0300

    fix: colocando gap correto

--- portifolio/src/components/projetos/Projetos.tsx
+++ portifolio/src/components/projetos/Projetos.tsx
@@ -4,7 +4,7 @@ import PrincipaisProjetos from "./PrincipaisProjetos"
 export default function Projeto() {
     
     return (
-        <section id="projetos" className="w-full lg:w-3/4 lg:max-w-screen-lg flex flex-col gap-4 items-center font-inter">
+        <section id="projetos" className="w-full lg:w-3/4 lg:max-w-screen-lg flex flex-col gap-8 items-center font-inter">
             <h2 className="w-full text-4xl font-bold md:text-center">Projetos</h2>
             <p className=" text-lg text-justify indent-8">Aqui você encontra uma seleção dos trabalhos que desenvolvi ao longo da minha trajetória. Cada projeto representa um desafio único, onde pude aplicar minhas habilidades, criatividade e aprendizado contínuo.
             </p>
