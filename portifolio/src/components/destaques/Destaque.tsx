4099fb73bf5037cdeb9c9a3af11a7729e4c08056
Yan Chagas <yanchagas2004@gmail.com>
2025-03-30 13:33:57 -0300

    feat: Destaques com formato padrão pronto

--- portifolio/src/components/destaques/Destaque.tsx
+++ portifolio/src/components/destaques/Destaque.tsx
@@ -7,7 +7,17 @@ type DestaquesProps = {
 
 export default function Destaque(props: DestaquesProps) {
     return (
-        <></>
+        <a className={`w-full max-w-72 h-[450px] flex flex-col flex-grow items-center bg-[#3333337c] hover:scale-105 duration-150 ease-in-out transition-all font-inter rounded-xl border-2 border-transparent hover:border-indigo-500`} href={props.link_github}>
+            <div className="flex justify-center w-full h-3/5">
+                <img className="w-full object-cover rounded-t-xl" src={props.imagem} alt={props.titulo} />
+            </div>
+            <div className="w-full h-2/5 flex flex-col p-4">
+                <h3 className="w-full text-lg font-bold text-left h-fit">{props.titulo}</h3>  
+                <div className="flex w-full h-full p-2 overflow-scroll">
+                    <p className="w-full text-justify break-words text-ellipsis h-full">{props.descricao}</p>
+                </div>
+            </div>
+        </a>
     )
 
 }
--- portifolio/src/components/destaques/Destaques.tsx
+++ portifolio/src/components/destaques/Destaques.tsx
@@ -1,7 +1,18 @@
+import Destaque from "./Destaque";
+
+const destaques = [
+    {titulo: "Geração de Tickets", descricao: "Página de geração de tickets proposta para a matéria de FullStack visando aplicar as habilidades aprendidas de HTML, CSS e JS aprendidas em sala.", link_github: "https://github.com/yan-campelo/Projeto1", imagem: "https://placehold.co/150x150"},
+    {titulo: "Calculadora de Idade", descricao: "Calculadora de idade proposta para a matéria de FullStack visando aplicar as habilidades aprendidas utilizando React, Node.js e TailwindCSS.", link_github: "https://github.com/yan-campelo/Projeto2", imagem: "https://placehold.co/150x200"},
+    {titulo: "MelembraAI", descricao: "Projeto futuro da materia de FullStack visando aplicar as habilidades aprendidas utilizando React, Node.js, TailwindCSS e requisição a APIs externas.", link_github: "https://github.com/yan-campelo/Projeto3", imagem: "https://placehold.co/150x150"}
+]
+
 export default function Destaques() {
     return (
-        <section className="w-full py-4 px-8 flex flex-col gap-4 items-center font-inter h-fit">
+        <section className="w-full py-4 px-8 flex flex-wrap justify-center items-center font-inter h-fit">
             <h2 className="w-full text-2xl font-bold">Destaques</h2>
+            <div className="w-full max-w-full flex flex-wrap justify-center items-end gap-6 p-4 h-full">
+                {destaques.map((card) => {return <Destaque key={card.titulo} titulo={card.titulo} descricao={card.descricao} link_github={card.link_github} imagem={card.imagem}/>})}
+            </div>
         </section>
     )
 }
