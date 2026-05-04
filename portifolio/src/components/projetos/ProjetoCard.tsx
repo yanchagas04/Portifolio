c22b767addbb7ffbd3a6774e7c53d6ee35f6be5a
Yan - Notebook Senai <yan.campelo@fbest.ogr.br>
2026-05-04 08:28:49 -0300

    fix: outros projetos

--- portifolio/src/components/projetos/ProjetoCard.tsx
+++ portifolio/src/components/projetos/ProjetoCard.tsx
@@ -1,12 +1,17 @@
 export type ProjetoProps = {
-   text: string,
-   link: string 
-}
+	text: string;
+	link: string;
+};
 
 export function ProjetoCard(props: ProjetoProps) {
-    return (
-        <div className="w-full flex flex-col gap-4 items-center duration-150 ease-in-out transition-all font-inter hover:animate-pulse">
-            <a className="text-md w-full flex flex-row hover:font-bold hover:text-indigo-500 transition-all " href={props.link}>{"- " + props.text}</a>
-        </div>
-    )
+	return (
+		<div className="w-full flex flex-col gap-4 items-center duration-150 ease-in-out transition-all font-inter hover:animate-pulse">
+			<a
+				className="text-md w-full flex flex-row hover:font-bold hover:text-vibrant-blue-100 transition-all "
+				href={props.link}
+			>
+				{"- " + props.text}
+			</a>
+		</div>
+	);
 }
